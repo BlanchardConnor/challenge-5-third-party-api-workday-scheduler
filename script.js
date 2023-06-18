3553a0cad977398075d6627a80b2c7b5dae677f7
BlanchardConnor <BlanchardConnor73@gmail.com>
2023-06-18 15:47:41 -0500

    added basic boilerplate functions to add and get items to/from local storage in JS

--- script.js
+++ script.js
@@ -8,7 +8,36 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  
+// Function to add an item to local storage
+function addToLocalStorage(key, value) {
+  if (typeof key !== 'string' || typeof value !== 'string') {
+    console.error('Key and value must be strings');
+    return;
+  }
+
+  localStorage.setItem(key, value);
+  console.log(`Item '${key}' successfully added to local storage.`);
+}
+
+// Function to retrieve an item from local storage
+function getFromLocalStorage(key) {
+  if (typeof key !== 'string') {
+    console.error('Key must be a string');
+    return;
+  }
+
+  const value = localStorage.getItem(key);
+  if (value === null) {
+    console.log(`Item '${key}' not found in local storage.`);
+    return;
+  }
+
+  console.log(`Item '${key}' retrieved from local storage: ${value}`);
+  return value;
+}
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
